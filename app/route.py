1acde064d5170fcdc5e9508a18c4445f536df5cf
MSadeghzadehG <mahdisadeghzadeh24@gmail.com>
2023-09-01 08:32:39 +0330

    Remove import.

--- app/route.py
+++ app/route.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask, request, jsonify, make_response
-from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 import uuid
 import jwt
